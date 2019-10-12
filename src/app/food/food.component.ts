29cfcd919c8d328ae95cc0def31250eb9a82e4b4
Natalie Craun <craun@usc.edu>
2019-10-11 23:31:36 -0500

    Begin working on food comp

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { MatButtonToggleChange } from '@angular/material';
 
 @Component({
   selector: 'app-food',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodComponent implements OnInit {
 
+  selectedValue = 'drinks';
+  drinks = 'drinks';
+  savory = 'savory';
+  desserts = 'desserts';
+  savorySelected = false;
+  drinksSelected = false;
+  dessertsSelected = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectionChanged(item) {
+    if (item.value === 'savory') {
+      this.savorySelected = true;
+      this.dessertsSelected = false;
+      this.drinksSelected = false;
+    } else if (item.value === 'desserts') {
+      this.dessertsSelected = true;
+      this.savorySelected = false;
+      this.drinksSelected = false;
+    } else {
+      this.drinksSelected = true;
+      this.savorySelected = false;
+      this.dessertsSelected = false;
+    }
   }
 
 }
