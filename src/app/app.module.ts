5361cc1f6bb44a552adf73d775b93610c193d931
Natalie Craun <craun@usc.edu>
2019-10-11 22:59:26 -0500

    Begin work on travel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule,
+         MatTabsModule,
+         MatButtonToggleModule,
+         MatIconModule,
+         MatGridListModule,
+         MatButtonModule} from '@angular/material';
+import { TravelComponent } from './travel/travel.component';
+import { FoodComponent } from './food/food.component';
+import { ArchitectureComponent } from './architecture/architecture.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelComponent,
+    FoodComponent,
+    ArchitectureComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonToggleModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
